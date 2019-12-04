b2e0359df0ca83909b28f3badfea38347599c284
B Severino <biancasev@gmail.com>
2019-12-04 15:44:44 -0500

    Styled search form

--- github-user-card/src/components/SearchForm.js
+++ github-user-card/src/components/SearchForm.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { TextField, Button } from '@material-ui/core';
+
+const useStyles = makeStyles({
+    form: {
+        width: 350,
+        height: 50,
+        padding: 20,
+        margin: '0 auto 20px',
+        background: '#FFF',
+        display: 'flex',
+        justifyContent: 'space-around',
+        alignItems: 'baseline',
+        borderRadius: 5
+    }
+});
 
 function SearchForm(props) {
+    const classes = useStyles();
+
     return (
-        <form onSubmit={props.changeUser}>
-            <input type='text' value={props.searchTerm} name='searchTerm' onChange={props.handleSearch} />
-            <button>Search User</button>
+        <form className={classes.form} onSubmit={props.changeUser}>
+            <TextField value={props.searchTerm} id='searchTerm' label='Username' onChange={props.handleSearch} />
+            <Button type='submit' variant='contained' color='primary'>Search User</Button>
         </form>
     );
 };
